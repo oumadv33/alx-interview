7b110c1ecedb29b17ca71447641baa00db6e861e
oumadv33 <alomkkalumq@gmail.com>
2024-08-08 09:35:16 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    Method that determines the number of minmum operations given n characters
+"""
+
+
+def minOperations(n):
+    """
+        A function that calculates the fewest number of operations
+        needed to give a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+        return:
+               number of min operations
+    """
